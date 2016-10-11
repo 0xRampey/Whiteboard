c5f1a8267cba8ecc22a89e30f258d37549c526e1
Prudhvi Rampey <gergteg777@gmail.com>
2016-10-12 00:32:20 +0530

    Phew. Added voice chat too

--- js/setup-canvas.js
+++ js/setup-canvas.js
@@ -1,3 +1,19 @@
+var startButton=document.getElementById("start");
+var webrtc;
+startButton.onclick=startVoice;
+  function startVoice(){
+  var webrtc = new SimpleWebRTC({
+  // the id/element dom element that will hold "our" video
+  media: { video: false, audio: true },
+  // immediately ask for camera access
+  autoRequestMedia: true
+});
+
+webrtc.on('readyToCall', function () {
+  // you can name it anything
+  webrtc.joinRoom('test');
+});
+}
 var canvasDiv1 = document.getElementById('canvasDiv');
 var numCanvas = 0;
 var canvas1 = new Canvas(canvasDiv1);
